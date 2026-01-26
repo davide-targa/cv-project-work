086c892ec8e05cb4e5ae5f6a6c9c1f4a59637b31
Davide Targa <davide.targa@gmail.com>
2026-01-26 14:10:19 +0100

    Add fasterrcnn_resnet50_v2.

---
+++ src/models/fasterrcnn_resnet50_v2.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from utils.cv_logger import logging
+
+logger = logging.getLogger(f"cv.{Path(__file__).stem}")
+
+
+def get_model():
+    model = fasterrcnn_resnet50_fpn_v2(weights="DEFAULT")
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes=2)
+    return model
