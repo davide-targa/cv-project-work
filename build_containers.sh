52940d0ce77ec271427cdaa92b9d784ac3ad9e9d
Davide Targa <davide.targa@gmail.com>
2026-01-27 08:51:58 +0100

    Fix build script.

--- build_containers.sh
+++ build_containers.sh
@@ -2,6 +2,6 @@
 
 module load apptainer
 
-apptainer build copernico/train_ssd300_vgg16.sif train_ssd300_vgg16.def
-apptainer build copernico/train_fasterrcnn_resnet50.sif train_fasterrcnn_resnet50.def
-apptainer build copernico/train_fasterrcnn_resnet50_v2.sif train_fasterrcnn_resnet50_v2.def
+apptainer build train_ssd300_vgg16.sif copernico/train_ssd300_vgg16.def
+apptainer build train_fasterrcnn_resnet50.sif copernico/train_fasterrcnn_resnet50.def
+apptainer build train_fasterrcnn_resnet50_v2.sif copernico/train_fasterrcnn_resnet50_v2.def
