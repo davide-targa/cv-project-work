4181df8ae80142e7114a6f4c2a3b1b20069f1720
Davide Targa <davide.targa@unife.it>
2026-01-27 08:17:46 +0100

    Add apptainer build script.

---
+++ copernico/build_containers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apptainer build copernico/train_ssd300_vgg16.sif copernico/train_ssd300_vgg16.def
+apptainer build copernico/train_fasterrcnn_resnet50.sif copernico/train_fasterrcnn_resnet50.def
+apptainer build copernico/train_fasterrcnn_resnet50_v2.sif copernico/train_fasterrcnn_resnet50_v2.def
