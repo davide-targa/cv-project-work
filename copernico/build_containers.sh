4d568100ab288f4c28616f8796374a8fcec563a3
Davide Targa <davide.targa@unife.it>
2026-01-27 08:19:43 +0100

    Fix apptainer build script.

--- copernico/build_containers.sh
+++ copernico/build_containers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+module load apptainer
+
 apptainer build copernico/train_ssd300_vgg16.sif copernico/train_ssd300_vgg16.def
 apptainer build copernico/train_fasterrcnn_resnet50.sif copernico/train_fasterrcnn_resnet50.def
 apptainer build copernico/train_fasterrcnn_resnet50_v2.sif copernico/train_fasterrcnn_resnet50_v2.def
