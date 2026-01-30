149f92f9856e1fca9257ece89c1f71ff6fca9886
Davide Targa <davide.targa@gmail.com>
2026-01-30 08:57:41 +0100

    Edit ssd300_vgg16.

--- src/models/ssd300_vgg16.py
+++ src/models/ssd300_vgg16.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from torchvision.models.detection import ssd300_vgg16
+from torchvision.models.detection import _utils, ssd300_vgg16
+from torchvision.models.detection.ssd import SSDClassificationHead
 
 from utils.cv_logger import logging
 
@@ -9,4 +10,14 @@ logger = logging.getLogger(f"cv.{Path(__file__).stem}")
 
 def get_model():
     model = ssd300_vgg16(weights="DEFAULT")
+    in_channels = _utils.retrieve_out_channels(model.backbone, (300, 300))
+    num_anchors = model.anchor_generator.num_anchors_per_location()
+    model.head.classification_head = SSDClassificationHead(
+        in_channels=in_channels,
+        num_anchors=num_anchors,
+        num_classes=2,
+    )
+    import pdb
+
+    pdb.set_trace()
     return model
